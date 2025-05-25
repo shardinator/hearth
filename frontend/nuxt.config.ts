2f06a05576d4c40125e744c929ac5f28cad166a7
Ankur Sharda <sharda.anks@gmail.com>
2025-05-25 14:14:22 +0800

    improved nuxt.config.ts

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
+  ssr: true,
+  target: 'server',
   devtools: { enabled: true },
   devServer: {
     port: 3000
